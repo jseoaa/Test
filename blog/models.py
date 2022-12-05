ee660edbfe7c50ea5dfb9dbc2522b452a3c8090f
jseoaa <jseoaa@naver.com>
2022-12-05 07:15:00 +0000

    Post 3

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+# 함수를 사용하는 방법과 class를 사용하는 방법
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
+    
+    
+    
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+    # author : 추후 작성 예정 
 # Create your models here.
