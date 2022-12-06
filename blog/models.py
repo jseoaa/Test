f9c7450d4f376030006a690dc007bd6df7ace662
jseoaa <jseoaa@naver.com>
2022-12-06 03:48:01 +0000

    FBV

--- blog/models.py
+++ blog/models.py
@@ -13,5 +13,10 @@ class Post(models.Model):
     
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+    
+    
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
+        
     # author : 추후 작성 예정 
 # Create your models here.
