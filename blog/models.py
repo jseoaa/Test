101716f2c9f92fe030d19c71027ec269c36915e4
jseoaa <jseoaa@naver.com>
2022-12-12 06:23:31 +0000

    list -dtail tag

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
-# 함수를 사용하는 방법과 class를 사용하는 방법
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    class Meta:
+        verbose_name_plural = 'categories'
+        
+        
+        
+        
+class Tag(models.Model):
+        name = models.CharField(max_length=50)
+        slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+        def __str__(self):
+            return self.name
+
+        def get_absolute_url(self):
+            return f'/blog/category/{self.slug}/'
+
+
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
-    
-    create_at = models.DateTimeField(auto_now_add=True)
-    updated_at =models.DateTimeField(auto_now=True)
-    
+
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    tags = models.ManyToManyField(Tag, blank=True)
     
     
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
-    
-    
+        return f'[{self.pk}]{self.title} :: {self.author}'
+
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
-        
-    # author : 추후 작성 예정 
-# Create your models here.
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
+
+    
+    
+    
--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from . import views 
+from . import views
 
-urlpatterns = [ 
-    # path('',views.index),
-    # #는 views.py 의 index 함수를 PostList 클래스로 대체해서 주석
-    #/blog 뒤에 아무것도 안붙이면 여기로 넘어옴
-    path('', views.PostList.as_view()),
-    # PostList 를 쓰면 
-    # template 로 갈땐 post_list.html으로 간다 
-    
-
-    path('<int:pk>/', views.PostDetail.as_view())
+urlpatterns = [
+    path('<int:pk>/',views.PostDetail.as_view()),
+    path('category/<str:slug>/',views.category_page),
+    # https://project-ztsct.run.goorm.io/blog/category/programming
+    # programming만 떼어 views.py의 category_page()
+    path('',views.PostList.as_view()),
     
 ]
